244633b07eab143c264a07fbd771c45aade3fa8c
mariorz <mariorz@gmail.com>
2009-05-30 22:26:51 -0500

    initial commit

---
+++ algtest.py
@@ -0,0 +1,201 @@
+
+from glpkwrap import GoalProblem
+import unittest
+
+
+class AlgebraTest(unittest.TestCase):
+    
+  
+    def test_sum_var_int(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        f = x1 + 6
+        self.assertEqual(f[x1], 1)
+        self.assertEqual(f.constant, 6)
+
+    def test_sum_int_var(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        f = 2 + x1
+        self.assertEqual(f[x1], 1)
+        self.assertEqual(f.constant, 2)
+
+
+    def test_sum_exp_int(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        f = x1 + 6
+        g = f + 3
+        self.assertEqual(g[x1], 1)
+        self.assertEqual(g.constant, 9)
+
+
+    def test_sum_int_exp(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        g = x1 + 4
+        f = 2 + g
+        self.assertEqual(f[x1], 1)
+        self.assertEqual(f.constant, 6)
+
+    def test_sum_var_exp(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        x2 = prob.variable("x2",0)
+        f = x1 + 6
+        g = x2 + f 
+        self.assertEqual(g[x1], 1)
+        self.assertEqual(g[x2], 1)
+        self.assertEqual(g.constant, 6)
+
+    def test_sum_exp_var(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        x2 = prob.variable("x2",0)
+        f = x1 + 6
+        g = f + x2 
+        self.assertEqual(g[x1], 1)
+        self.assertEqual(g[x2], 1)
+        self.assertEqual(g.constant, 6)
+
+    def test_sum_var_var(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        x2 = prob.variable("x2",0)
+        f = x1 + x2
+        self.assertEqual(f[x1], 1)
+        self.assertEqual(f[x2], 1)
+
+
+    def test_sum_exp_exp(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        x2 = prob.variable("x2",0)
+        g = x1 + 4
+        h = x2 + 3
+        f = g + h
+        self.assertEqual(f[x1], 1)
+        self.assertEqual(f[x2], 1)
+        self.assertEqual(f.constant, 7)
+
+
+    def test_sub_var_int(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        f = x1 - 3
+        self.assertEqual(f[x1], 1)
+        self.assertEqual(f.constant, -3)
+
+    
+    def test_sub_int_var(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        f = 8 - x1
+        self.assertEqual(f[x1], -1)
+        self.assertEqual(f.constant, 8)
+
+    def test_sub_exp_int(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        f = x1 + 6
+        g = f - 3
+        self.assertEqual(g[x1], 1)
+        self.assertEqual(g.constant, 3)
+
+    
+    def test_sub_int_exp(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        g = x1 + 4
+        f = 2 - g
+        self.assertEqual(f[x1], -1)
+        self.assertEqual(f.constant, -2)
+
+
+    def test_sub_var_exp(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        x2 = prob.variable("x2",0)
+        f = x1 + 6
+        g = x2 - f 
+        self.assertEqual(g[x1], -1)
+        self.assertEqual(g[x2], 1)
+        self.assertEqual(g.constant, -6)
+
+    def test_sub_exp_var(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        x2 = prob.variable("x2",0)
+        f = x1 + 6
+        g = f - x2 
+        self.assertEqual(g[x1], 1)
+        self.assertEqual(g[x2], -1)
+        self.assertEqual(g.constant, 6)
+        
+   
+    def test_sub_var_var(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        x2 = prob.variable("x2",0)
+        f = x1 - x2
+        self.assertEqual(f[x1], 1)
+        self.assertEqual(f[x2], -1)
+
+
+
+    def test_sub_exp_exp(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        x2 = prob.variable("x2",0)
+        g = x1 + 4
+        h = x2 + 3
+        f = g - h
+        self.assertEqual(f[x1], 1)
+        self.assertEqual(f[x2], -1)
+        self.assertEqual(f.constant, 1)
+  
+
+    def test_mult_var_int(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        f = x1 * 2
+        self.assertEqual(f[x1], 2)
+        self.assertEqual(f.constant, 0)
+        
+
+    def test_mult_int_var(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        f = 2 * x1
+        self.assertEqual(f[x1], 2)
+        self.assertEqual(f.constant, 0)
+
+    def test_mult_exp_int(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        g = x1 + 2
+        f = g * 7
+        self.assertEqual(f[x1], 7)
+        self.assertEqual(f.constant, 14)
+        
+
+    def test_mult_int_exp(self):
+        prob = GoalProblem('unit test')
+        x1 = prob.variable("x1",0)
+        g = x1 + 4
+        f = 2 * g
+        self.assertEqual(f[x1], 2)
+        self.assertEqual(f.constant, 8)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()   
+
+
+
+
---
+++ glpkwrap.py
@@ -0,0 +1,140 @@
+
+import glpk
+import unittest
+
+class GoalProblem(object):
+    def __init__(self, name):
+        self.lp = glpk.LPX()        
+        self.lp.name = name     
+        self.lp.obj.maximize = False
+    def variable(self, name, lower=None, upper=None):
+        count = len(self.lp.rows)
+        self.lp.rows.add(1)
+        row = self.lp.rows[count]
+        row.name = name
+        row.bounds = lower, upper
+        var = GoalVariable(name, lower, upper)
+        return var
+    def objective(self, eq):
+        if isinstance(eq, GoalVariable):
+            eq = GoalExpresion(eq)
+    def priority(self, eq):
+        pass
+    def solve(self):
+        pass
+    def display(self):
+        pass
+    
+
+class GoalVariable(object):
+    def __init__(self, name, lower=None, upper=None):
+        self.name = name
+        self.lower = lower
+        self.upper = upper
+    def __add__(self, other):
+        return GoalExpresion(self) + other
+    def __radd__(self, other):
+        return GoalExpresion(self) + other
+    def __sub__(self, other):
+        return GoalExpresion(self) - other
+    def __rsub__(self, other):
+        return other - GoalExpresion(self)
+    def __mul__(self, other):
+        return GoalExpresion(self) * other
+    def __rmul__(self, other):
+        return GoalExpresion(self) * other
+    def __pos__(self):
+        return self
+    def __neg__(self):
+        return GoalExpresion(self) * -1
+    def __eq__(self, other):
+        return GoalExpresion(self) == other
+
+
+
+class GoalExpresion(dict):
+    def __init__(self, e=None, cons=0):
+        if isinstance(e, GoalExpresion):
+            self.constant = e.constant
+            dict.__init__(self, e)
+        elif isinstance(e, GoalVariable):
+            self.constant = 0
+            dict.__init__(self, {e:1})
+        else:
+            self.constant = 0
+            dict.__init__(self)
+    def __neg__(self):
+        e = GoalExpresion(self)
+        return e * -1
+    def __add__(self, other):
+        return self.addop(other)
+    def __radd__(self, other):
+        return self.addop(other)
+    def __sub__(self, other):
+        return self.addop(-other)
+    def __rsub__(self, other):
+        return (-self).addop(other)
+    def __mul__(self, other):
+        e = GoalExpresion()
+        if isinstance(other, int):
+            if other != 0:
+                e.constant = self.constant * other
+                for v,x in self.iteritems():
+                        e[v] = x * other
+        elif isinstance(other, GoalVariable):
+            return self * GoalExpresion(other)
+        elif isinstance(other, GoalExpresion):
+            raise TypeError, "Non-constant expressions cannot be multiplied in LP"
+        return e
+    def __rmul__(self, other):
+        return (self * other)
+    def __eq__(self, other):
+        return GoalObjective(self - other)
+    def addop(self, other):
+        e = GoalExpresion(self)
+        if other is 0: return e
+        if isinstance(other, int):
+            e.constant += other
+        elif isinstance(other, GoalVariable):
+            e.addterm(other, 1)
+        elif isinstance(other, GoalExpresion):
+            e.constant += other.constant
+            for v,x in other.iteritems():
+                e.addterm(v, x)
+        return e
+    
+    def addterm(self, key, value):
+        y = self.get(key, 0)
+        if y:
+            y += value
+            if y: self[key] = y
+            else: del self[key]
+        else:
+            self[key] = value
+
+
+
+
+"""
+    def addterm(self, key, value):
+        y = self.get(key, 0)
+        if y:
+            y += value
+            if y: self[key] = y
+            else: del self[key]
+        else:
+            self[key] = value
+
+
+"""
+    
+
+class GoalObjective(object):
+    pass
+
+
+class GoalPriority(object):
+    pass
+
+
+
