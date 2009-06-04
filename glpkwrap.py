b56b323784734923ee73dc3457393d3c19d98352
mariorz <mariorz@gmail.com>
2009-06-04 00:49:08 -0500

    licence

--- glpkwrap.py
+++ glpkwrap.py
@@ -1,9 +1,28 @@
+"""
+Copyright (C) 2009 Mario Romero, mariorz@gmail.com
+
+This file is part of Pytorovich.
+
+Pytorovich is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Pytorovich is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Pytorovich.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 
 import glpk
 
 #TO DO:
 
-# new class names
+# new class names 
 # better print_results
 # docs
 
@@ -321,4 +340,7 @@ class LinearEquation(dict):
         y += value
         self[key] = y
         
+
+
+
 
