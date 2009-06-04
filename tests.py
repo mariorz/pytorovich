72a6e4bbc72f76600c32b4be4e9c9618b0c9f88f
mariorz <mariorz@gmail.com>
2009-06-04 00:51:14 -0500

    licence 2

--- tests.py
+++ tests.py
@@ -1,3 +1,22 @@
+"""
+Copyright (C) 2009 Mario Romero, mariorz@gmail.com
+
+This file is part of Pytorovich.
+
+Pytorovich is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Pytorovich is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Pytorovich.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 
 from glpkwrap import LinearProblem
 import unittest
