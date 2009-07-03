ba366f053c9b36a43950481d1a83a9cb7a49152d
mariorz <mariorz@gmail.com>
2009-07-02 22:10:36 -0500

    init setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+
+
+from distutils.core import setup 
+import sys, os, os.path
+
+
+setup(name = 'pytorovich',
+      version = '0.1.1',
+      description = 'Pytorovich is a PyGLPK wrapper for solving Linear Programming problems in a civilized manner. ',
+      author = 'Mario Romero',
+      author_email = 'm@marioromero.org',
+      url = 'http://github.com/mariorz/pytorovich',
+      license = 'GPL',
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Operating System :: Platform Independent',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules' ],
+      py_modules=['pytorovich']
+)
+
+
